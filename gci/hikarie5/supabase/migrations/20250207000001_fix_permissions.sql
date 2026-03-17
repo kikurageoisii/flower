7166c4273441a1e6156406297cb3b13e4111abae
kikurageoisii <stringb109@gmail.com>
2026-03-17 13:05:12 +0900

    Delete gci/hikarie5 directory

--- gci/hikarie5/supabase/migrations/20250207000001_fix_permissions.sql
+++
@@ -1,4 +0,0 @@
--- Grant INSERT permissions to anon role for registration workaround
-GRANT INSERT ON users TO anon;
-GRANT INSERT ON preferences TO anon;
-GRANT INSERT ON personality_profiles TO anon;
